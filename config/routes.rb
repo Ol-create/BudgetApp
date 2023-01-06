4f0f2427f69b0ad44d15bec5cbc7de39ef034f9f
Oluyemi Paul <oluola96@gmail,com>
2023-01-06 13:39:20 +0100

    add auth with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root to: "home#index"
   resources :users
-  resources :entities
   resources :groups
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
